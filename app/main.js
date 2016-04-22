a3ac11d3ed98470f206e45e700dc750602913da3
Eleanor Demis <eleanordemis@gmail.com>
2016-04-22 09:04:08 -0700

    [setup] constructs routes

---
+++ app/main.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import Router from 'react-router';
+import ReactDOM from 'react-dom';
+import createBrowserHistory from 'history/lib/create';
+import routes from './routes';
