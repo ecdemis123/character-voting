d48f0a163f5a8633dbf5a6460445cec7509ca25c
Eleanor Demis <eleanordemis@gmail.com>
2016-04-23 21:16:29 -0700

    [setup] adds home component

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 'use strict'
+require('babel-register');
+const swig = require('swig');
+const React = require('react');
+const ReactDom = require('react-dom/server');
+const router = require('react-router');
+var routes = require('./app/routes');
 
 const express = require('express');
 const path = require('path');
@@ -12,6 +18,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res) => {
+  Router.match({routes: routes})
+})
 
 app.listen(app.get('port'), () => {
   console.log('listening on port', app.get('port'));
